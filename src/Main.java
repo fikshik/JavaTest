ea9c44e331788ba3182c4c67d570602c9036c3b4
fikshik <131162513+fikshik@users.noreply.github.com>
2023-04-23 14:57:00 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -21,13 +21,7 @@ class Main {
         if (operands.length != 2) throw new Exception("Должно быть два операнда");
         oper = detectOperation(expression);
         if (oper == null) throw new Exception("Это не математическая операция");
-        //если оба числа римские
-        if (Roman.isRoman(operands[0]) && Roman.isRoman(operands[1])) {
-            //приводим оба числа в арабские для вычесления
-            num1 = Roman.convertToArabian(operands[0]);
-            num2 = Roman.convertToArabian(operands[1]);
-            isRoman = true;
-        }
+      
         //если оба числа арабские
         else if (!Roman.isRoman(operands[0]) && !Roman.isRoman(operands[1])) {
             num1 = Integer.parseInt(operands[0]);
@@ -36,24 +30,17 @@ class Main {
         }
         //если одно число римское, а другое - арабское
         else {
-            throw new Exception("Числа должны быть в одном формате");
+            throw new Exception("Числа должны быть в арабские");
         }
         if (num1 > 10 || num2 > 10) {
             throw new Exception("Числа должны быть от 1 до 10");
         }
 
         int arabian = calculation(num1, num2, oper);
-        if (isRoman) {
-            //если римское число получилось меньше либо равно нулю, генерируем ошибку
-            if (arabian <= 0) {
-                throw new Exception("Римское число должно быть больше нуля");
-            }
-            //конвертируем результат операции из арабского в римское
-            result = Roman.convertToRoman(arabian);
-        } else {
-            //Конвертируем арабское число в тип String
-            result = String.valueOf(arabian);
-        }
+        
+        //Конвертируем арабское число в тип String
+        result = String.valueOf(arabian);
+        
         //возвращаем результат
         return result;
     }
